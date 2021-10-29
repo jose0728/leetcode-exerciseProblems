fb5985e4324e21dcb12ec3a0a030438aaab211c5
fanyu <fanyu@zhijiaxing.net>
2021-10-29 14:26:15 +0800

    大数打印

--- 大数打印/Program.cs
+++ 大数打印/Program.cs
@@ -9,11 +9,18 @@ namespace 大数打印
     class Program
     {
         /// <summary>
+        /// 剑指offer17题
         /// 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+        /// 示例 1:
+        /// 输入: n = 1
+        /// 输出: [1,2,3,4,5,6,7,8,9]
+        /// 说明：
+        /// 用返回一个整数列表来代替打印
+        /// n 为正整数
         /// </summary>
         static void Main(string[] args)
         {
-            int[] a = PrintNumbers2(3);
+            int[] a = PrintNumbers1(3);
         }
 
         public static int[] PrintNumbers1(int n)
@@ -22,7 +29,7 @@ namespace 大数打印
             int[] res = new int[len - 1];
             for (int i = 1; i < len; i++)
             {
-                res[i - 1] = i;
+                res[i-1] = i;
             }
             return res;
         }
